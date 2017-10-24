bda65d7d43a28ae11f9edd78e53bca8560fe0f8b
Josh Hubers <jhubers@oaisd.org>
2017-10-24 13:33:12 -0400

    Do the thingz

--- bchain.go
+++ bchain.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/google/jsonapi"
 	"log"
 	"net/http"
@@ -21,9 +22,21 @@ type Payload struct {
 	Info string `jsonapi:"attr,info"`
 }
 
+func (bchain *Chain) AddBlock(data Payload) []*Block {
+	previousBlock := bchain.Blocks[len(bchain.Blocks)-1]
+	newBlock := &Block{
+		Index:    previousBlock.Index + 1,
+		Data:     &data,
+		Previous: previousBlock,
+	}
+
+	bchain.Blocks = append(bchain.Blocks, newBlock)
+	return bchain.Blocks
+}
+
 func main() {
 
-	block := &Block{
+	block := Block{
 		Index: 0,
 		Data: &Payload{
 			Id:   0,
@@ -32,20 +45,37 @@ func main() {
 		Previous: nil,
 	}
 
-	chain := &Chain{
-		Blocks: []*Block{block},
+	testP := Payload{
+		Id:   1,
+		Info: "My new info",
 	}
 
-	http.HandleFunc("/block", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", jsonapi.MediaType)
-		if err := jsonapi.MarshalPayload(w, block); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
+	chain := Chain{
+		Blocks: []*Block{&block},
+	}
+
+	chain.AddBlock(testP)
+
+	fmt.Println(chain)
+
+	http.HandleFunc("/payload", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "POST" {
+			payload := new(Payload)
+
+			if err := jsonapi.UnmarshalPayload(r.Body, payload); err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			chain.AddBlock(*payload)
+			jsonapi.MarshalPayload(w, &chain)
+
 		}
 	})
 
 	http.HandleFunc("/chain", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", jsonapi.MediaType)
-		if err := jsonapi.MarshalPayload(w, chain); err != nil {
+		if err := jsonapi.MarshalPayload(w, &chain); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 	})
