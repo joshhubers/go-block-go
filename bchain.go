f146cb4450ccc6835c897cb4347398d5ed4f1155
Nick Beukema <nick.beukema@gmail.com>
2017-10-26 13:29:57 -0400

    Begin creating register endpoint

--- bchain.go
+++ bchain.go
@@ -4,7 +4,10 @@ import (
 	"fmt"
 	"github.com/google/jsonapi"
 	"log"
+	"net"
 	"net/http"
+	"os"
+	"bufio"
 )
 
 type Chain struct {
@@ -18,8 +21,17 @@ type Block struct {
 }
 
 type Payload struct {
-	Id   int    `jsonapi:"primary,payload"`
+	ID   int    `jsonapi:"primary,payload"`
 	Info string `jsonapi:"attr,info"`
+}
+
+type Node struct {
+	ID int `jsonapi:"primary,node"`
+	IP int `jsonapi:"attr,ip"`
+}
+
+type NodeList struct {
+	Nodes []*Node `jsonapi:"relation,nodes"`
 }
 
 func (bchain *Chain) AddBlock(data Payload) []*Block {
@@ -34,36 +46,70 @@ func (bchain *Chain) AddBlock(data Payload) []*Block {
 	return bchain.Blocks
 }
 
-func main() {
+func getIP() string {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		os.Stderr.WriteString("Oops: " + err.Error() + "\n")
+		os.Exit(1)
+	}
 
+
+	myIPs := []string{}
+
+	for _, a := range addrs {
+		if ipnet, ok := a.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				myIPs = append(myIPs, ipnet.IP.String())
+			}
+		}
+	}
+
+	if len(myIPs) == 0 {
+		os.Stderr.WriteString("You are not connected to a network.\n")
+		os.Exit(1)
+	}
+
+	return myIPs[0]
+}
+
+func determineStartState() string {
+	fmt.Println("Enter the IP address you wish to connect to, or leave blank to start your own chain")
+	reader := bufio.NewReader(os.Stdin)
+	text, _ := reader.ReadString('\n')
+
+	if text == "\n" {
+		return ""
+	} else {
+		return text
+	}
+}
+
+func generateGenesis() Chain {
 	block := Block{
 		Index: 0,
 		Data: &Payload{
-			Id:   0,
+			ID:   0,
 			Info: "This is my payload",
 		},
 		Previous: nil,
-	}
-
-	testP := Payload{
-		Id:   1,
-		Info: "My new info",
 	}
 
 	chain := Chain{
 		Blocks: []*Block{&block},
 	}
 
-	chain.AddBlock(testP)
+	return chain
+}
 
-	fmt.Println(chain)
-
+func beginServer(chain Chain) {
 	http.HandleFunc("/payload", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", jsonapi.MediaType)
+
 		if r.Method == "POST" {
 			previousBlock := chain.Blocks[len(chain.Blocks)-1]
 
 			payload := Payload{
-				Id: previousBlock.Data.Id + 1,
+				ID: previousBlock.Data.ID + 1,
 			}
 
 			if err := jsonapi.UnmarshalPayload(r.Body, &payload); err != nil {
@@ -76,10 +122,6 @@ func main() {
 			jsonapi.MarshalPayload(w, &chain)
 
 		}
-
-		if r.Method == "GET" {
-			jsonapi.MarshalPayload(w, &testP)
-		}
 	})
 
 	http.HandleFunc("/chain", func(w http.ResponseWriter, r *http.Request) {
@@ -89,5 +131,44 @@ func main() {
 		}
 	})
 
+	http.HandleFunc("/register", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", jsonapi.MediaType)
+		if r.Method == "POST" {
+			node := Node{
+				ID: 1,
+			}
+
+			if err := jsonapi.UnmarshalPayload(r.Body, &node); err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			fmt.Println(node)
+		}
+
+	})
+
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func goGetChain(targetIP string) Chain {
+	fmt.Println("Going to get Chain from " + targetIP)
+	chain := Chain{}
+	return chain
+}
+
+func main() {
+	myIP := getIP()
+	targetIP := determineStartState()
+
+	chain := Chain{}
+
+	if len(targetIP) == 0 {
+		chain = generateGenesis()
+	} else {
+		chain = goGetChain(targetIP)
+	}
+
+	fmt.Println("Your IP address for others to connect to is: " + myIP)
+	beginServer(chain)
 }
